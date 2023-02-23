9caf3cb4bb10fb171dfc2550db5516f4357377bf
fredriktre <trevlandf1331@outlook.com>
2023-02-23 14:12:01 +0100

    Input

--- Miniprojects/querymessenger/src/App.tsx
+++ Miniprojects/querymessenger/src/App.tsx
@@ -5,7 +5,7 @@ import PostMessage from './components/PostMessage'
 function App() {
 
   return (
-    <div className='flex justify-center items-center w-full h-full bg-gray-900'>
+    <div className='flex justify-center items-top w-full h-full bg-gray-900'>
       <div className="flex justify-center items-top gap-10 h-5/6 w-full">
         <GetMessage />
         <PostMessage />
