7c27d7d0b9ffcaa882d260ef7e03f9896a5c1ab7
fredriktre <trevlandf1331@outlook.com>
2023-06-13 08:23:41 +0200

    ss

--- Rust/tutorial-1/project-1/src/main.rs
+++ Rust/tutorial-1/project-1/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+
+    // let x = 4; // implicit type | compiler decides
+    // let x:u32 // not implicit
+
+    // let x = 4;
+    // println!("x is: {}", x) // for embedding. x will be put into {}
+
+    // let mut x = 4; | For making values mutable, can also "recreate" it. So just repeating let x = NEW_VALUE
+    // println!("x is: {}", x);
+
+    // x = 5;
+    // println!("x is: {}", x);
+
+    // { // this is a new scope
+    //     let x = 2;
+    //     println!("x is: {}", x);
+    // }
+
 }
