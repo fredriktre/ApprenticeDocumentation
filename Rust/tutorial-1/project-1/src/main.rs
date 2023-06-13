f80a24b0e9b2802a7a1dca4fc4aee46cca5d332d
fredriktre <trevlandf1331@outlook.com>
2023-06-13 08:28:16 +0200

    ss

--- Rust/tutorial-1/project-1/src/main.rs
+++ Rust/tutorial-1/project-1/src/main.rs
@@ -13,8 +13,11 @@ fn main() {
     // println!("x is: {}", x);
 
     // { // this is a new scope
+        // I can use the "parent" variables, but the parent can't use this scope's variable. So name shadowing is possible
     //     let x = 2;
     //     println!("x is: {}", x);
     // }
 
+    // const SECONDS_IN_MINUTE:u32 = 60; | This is a constant, you have to give it a value and type for it to work.
+    // println!("there are {} seconds in a minute", SECONDS_IN_MINUTE);
 }
