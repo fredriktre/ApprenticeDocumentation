b1a5d0c8050036f96799473ec8745ec6119eea57
fredriktre <trevlandf1331@outlook.com>
2023-08-22 14:53:33 +0200

    progress!

--- React/cv2023/src/App.tsx
+++ React/cv2023/src/App.tsx
@@ -1,43 +1,96 @@
 import { useEffect, useState } from 'react'
-import IMGBG1 from './assets/mountainbg.png'
+// import "./s1.css"
+import "./s2.css"
 
 function App() {
+  const [currentHoveredButton, setCurrentHoveredButton] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  const pages = [
+    {
+      name: "home",
+      image: "/assets/home.png"
+    },
+    {
+      name: "education",
+      image: "/assets/education.png"
+    },
+    {
+      name: "experience",
+      image: "/assets/experience.png"
+    },
+    {
+      name: "skillset",
+      image: "/assets/skillset.png"
+    },
+    {
+      name: "Portfolio",
+      image: "/assets/portfolio.png"
+    },
+    {
+      name: "AI stuff",
+      image: "/assets/aistuff.png"
+    },
+    {
+      name: "Freetime",
+      image: "/assets/freetime.png"
+    },
+  ]
 
-  // useEffect(() => {
-  //   console.log(currentPage)
-  // }, [currentPage])
+  const hackify = (id:string) => {
+    const button = document.getElementById(id)
+    let iterations = 0;
+    const interval = setInterval(() => {
+      button?.childNodes.forEach((childNode:any, index:number) => {
+        if (index < iterations) {
+          childNode.innerText = id.split("-")[0][index];
+          childNode.style = "color: #ffffff";
+        } else {
+          childNode.innerText = alphabet[Math.floor(Math.random() * alphabet.length)]
+          childNode.style = "color: #00ff00";
+        }
+      })
+      iterations += 1 / 6;
+      if (iterations > 10) clearInterval(interval);
+    }, 10)
+  }
 
   return (
-    <>
-
+    <div className='home-page'>
       <nav>
-
-        <button onClick={() => setCurrentPage(0)}
-        className={`${currentPage === 0 ? "active" : ""}`}>Home</button>
-        <button onClick={() => setCurrentPage(1)}
-        className={`${currentPage === 1 ? "active" : ""}`}>Education</button>
-        <button onClick={() => setCurrentPage(2)}
-        className={`${currentPage === 2 ? "active" : ""}`}>Work</button>
-        <button onClick={() => setCurrentPage(3)}
-        className={`${currentPage === 3 ? "active" : ""}`}>Skills</button>
-        <button onClick={() => setCurrentPage(4)}
-        className={`${currentPage === 4 ? "active" : ""}`}>Portfolio</button>
-        <button onClick={() => setCurrentPage(5)}
-        className={`${currentPage === 5 ? "active" : ""}`}>AI-Stuffs</button>
-        <button onClick={() => setCurrentPage(6)}
-        className={`${currentPage === 6 ? "active" : ""}`}>Freetime</button>
-
-        <span></span>
+          {
+            pages.map((page:any, index:number) => (
+              <>
+                <button id={`${page.name}-${index}`} key={index} onClick={() => setCurrentPage(index)}
+                onMouseEnter={() => {
+                  hackify(`${page.name}-${index}`);
+                  setCurrentHoveredButton(index + 1)
+                }}
+                onMouseLeave={() => setCurrentHoveredButton(0)}
+                >
+                  {
+                    page.name.split("").map((letter:string, index:number) => (
+                      <span key={`${letter}-${index}`}>{letter}</span>
+                    ))
+                  }
+                </button>
+                <img src={page.image} className={`${currentHoveredButton === index + 1 ? "active" : "inactive"}`} />
+              </>
+            ))
+          }
       </nav>
+      
+      <div className='content-container'>
+          <div className={`content-wrapper ${currentPage != 0 ? "active" : ""}`}>
 
-      <div className='card content-card'>
-        <div className='content-card-content'>
+            
 
-        </div>
-        <span></span>
+          </div>
       </div>
-    </>
+
+      <span></span>
+    </div>
   )
 }
 
